46d39f94803a3716df84b83f00af04f7553e085b
Cesar Gutierrez <langsikp@gmail.com>
2020-04-21 14:59:06 -0500

    Ejercicio float

---
+++ ejercicio1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	float numero, resultado;
+	
+	cout<<"Ingrese un numero: "<<endl;
+	cin>>numero;
+	
+	resultado = numero / 100;
+	
+	cout<<"El resultado es: "<<resultado;
+	
+	return 0;
+}
